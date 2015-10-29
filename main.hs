3a5ed7decc214d73155c83e19f245bf464342d1c
arrowrowe <arrowrowe@gmail.com>
2015-10-29 23:35:59 +0800

    Progress step 2: whitespace

--- main.hs
+++ main.hs
@@ -1,12 +1,15 @@
 module Main where
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+
+spaces :: Parser ()
+spaces = skipMany1 space
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
